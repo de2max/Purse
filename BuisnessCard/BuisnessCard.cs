8db43641a7dd3bb70c70d7fdb0fe5d899bdce1b0
de2max <44652756+de2max@users.noreply.github.com>
2020-05-26 11:01:06 +0300

    BuisnessCard

---
+++ BuisnessCard/BuisnessCard.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace BuisnessCardLib
+{
+    public class BuisnessCard
+    {
+        protected double amount { get; set; }
+
+        public BuisnessCard(double sum) {
+            this.amount = sum;
+        }
+
+        public double info() {
+            return this.amount;
+        }
+    }
+}
--- MyPurse/Program.cs
+++ MyPurse/Program.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using BuisnessCardLib;
 
 namespace MyPurse
 {
@@ -10,6 +11,11 @@ namespace MyPurse
     {
         static void Main(string[] args)
         {
+            BuisnessCard bcard = new BuisnessCard(101);
+
+            Console.WriteLine(bcard.info());
+
+            Console.ReadKey();
         }
     }
 }
